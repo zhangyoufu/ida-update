f0ca1f6c5d0a969060a019dc71efca8e6b9c23b8
张酉夫 <zhangyoufu@gmail.com>
2020-02-04 09:11:41 +0800

    adapt new version

--- .github/cron.sh
+++ .github/cron.sh
@@ -2,7 +2,7 @@
 set -ex
 set -o pipefail
 
-curl --silent --show-error https://www.hex-rays.com/updida.shtml | python -c "import re, sys; sys.stdout.write(re.search(r'(?s)(?<=<center>\n      ).*(?=    </center>)', sys.stdin.read()).group().replace('<br>', '').replace('\n ', ''))" >version.txt
+curl --silent --show-error https://www.hex-rays.com/updida/ | python -c "import re, sys; sys.stdout.write(re.search(r'(Latest available versions:) <strong>(IDA: .*?)</strong> &ndash; <strong>(Decompiler: .*?)</strong>', sys.stdin.read()).expand(r'\1\n\2\n\3'))" >version.txt
 
 if [ -n "$(git status --porcelain)" ]; then
     pip3 install -r .github/requirements.txt
